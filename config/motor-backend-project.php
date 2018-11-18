963a516175217eb89b5ac45186a14a144b82c36d
Reza Esmaili <github@dfox.info>
2018-11-18 01:34:19 +0100

    Small css/html fixes

--- config/motor-backend-project.php
+++ config/motor-backend-project.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'name'        => 'Motor CMS',
-    'name_part_1' => 'MOTOR',
-    'name_part_2' => 'CMS',
-    'name_short'  => 'M',
+    'name'        => 'PartyMeister 3.0',
+    'name_part_1' => 'PM3',
+    'name_part_2' => '',
+    'name_short'  => 'PM',
     'copyright'   => 'Reza Esmaili',
-    'logo-large'  => 'images/motor-logo-large.png',
-    'logo-small'  => 'images/motor-logo-small.png',
-    'logo-white'  => 'images/motor-logo-small-white.png'
+    'logo-large'  => 'images/pm-logo-large.png',
+    'logo-small'  => 'images/pm-logo-small.png',
+    'logo-white'  => 'images/pm-logo-small-white.png'
 ];
--- resources/views/vendor/motor-backend/layouts/partials/footer.blade.php
+++ resources/views/vendor/motor-backend/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Footer -->
 <footer class="app-footer">
-    <span><a href="https://partymeister.org">PartyMeister</a> © 2017 Reza Esmaili</span>
+    <span><a href="https://partymeister.org">PartyMeister</a> © 2005 &ndash; {{date('Y')}} Reza Esmaili</span>
     <span class="ml-auto">Powered by <a href="https://motor-cms.com">Motor CMS</a></span>
 </footer>
