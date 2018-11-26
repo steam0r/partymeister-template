b8161b26957bccfa82098371c80cbf8afefbbd24
Reza Esmaili <github@dfox.info>
2018-11-26 01:36:41 +0100

    Add merge script to merge different package.json files into one master file

---
+++ merge-package-json.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+package-json-merge package.json.base packages/dfox288/motor-cms/resources/assets/npm/package.json packages/dfox288/motor-backend/resources/assets/npm/package.json packages/dfox288/partymeister-accounting/resources/assets/npm/package.json packages/dfox288/partymeister-frontend/resources/assets/npm/package.json packages/dfox288/partymeister-slides/resources/assets/npm/package.json > package.json
