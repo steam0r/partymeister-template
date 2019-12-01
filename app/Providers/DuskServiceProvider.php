9788815d0e724ba1ef34532886669145751a5545
Reza Esmaili <github@dfox.info>
2019-12-01 23:47:46 +0100

    Small fixes and dependency updates

--- app/Providers/DuskServiceProvider.php
+++ app/Providers/DuskServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\Browser;
 
 class DuskServiceProvider extends ServiceProvider
 {
+
     /**
      * Register the Dusk's browser macros.
      *
@@ -14,10 +16,12 @@ class DuskServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Browser::macro('scrollToElement', function ($element = null) {
-            $this->script("$('html, body').animate({ scrollTop: $('$element').offset().top }, 0);");
+        if (App::environment('testing')) {
+            Browser::macro('scrollToElement', function ($element = null) {
+                $this->script("$('html, body').animate({ scrollTop: $('$element').offset().top }, 0);");
 
-            return $this;
-        });
+                return $this;
+            });
+        }
     }
 }
