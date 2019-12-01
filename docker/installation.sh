cb7ae4baf0dabfff054197b04b54f08893e59927
Reza Esmaili <github@dfox.info>
2019-12-02 00:58:44 +0100

    Add instructions for after installation

--- docker/installation.sh
+++ docker/installation.sh
@@ -4,3 +4,5 @@ export $(grep -v '^#' .env | xargs)
 
 # Create database
 docker exec -i ${CONTAINER_NAME} /bin/bash /tmp/pm-installation.sh
+
+echo "Done - please change APP_ENV in .env to production and restart the container"
