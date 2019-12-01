facaa3de668b1c3726e4fa1d68b6b28ff7097b75
Reza Esmaili <github@dfox.info>
2019-12-02 00:43:02 +0100

    Only start webdriver in docker after composer install

--- docker/webserver/config/init.sh
+++ docker/webserver/config/init.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-su application -c "php /app/artisan partymeister:slides:webdriver start"
+if [ -d "/app/vendor" ]; then
+  su application -c "php /app/artisan partymeister:slides:webdriver start"
+fi
