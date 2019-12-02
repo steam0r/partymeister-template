f9f2c06be90e16643d1fc1b26624454ee28e3a9e
Reza Esmaili <github@dfox.info>
2019-12-02 01:14:21 +0100

    Update dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.6.0 on 2019-12-02 00:25:28.
+ * Generated for Laravel 6.6.0 on 2019-12-02 01:14:13.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
