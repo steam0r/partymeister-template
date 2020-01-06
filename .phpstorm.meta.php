4073fa4c31836dc4657574b3adc0a9fe1aa6c072
Reza Esmaili <bitbucket@dfox.info>
2020-01-07 00:15:48 +0100

    Update assets

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-12-27 02:23:08.
+    * Generated on 2020-01-01 17:58:03.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.9.0 on 2019-12-27 02:23:08.
+ * Generated for Laravel 6.9.0 on 2020-01-01 17:58:03.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
