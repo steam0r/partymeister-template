993d84ef67b21f39ba94893b96fa7a2817089c5e
Reza Esmaili <bitbucket@dfox.info>
2020-01-16 22:20:49 +0100

    Add 2020 minimal template

--- resources/views/vendor/partymeister-frontend/layouts/frontend/partymeister.blade.php
+++ resources/views/vendor/partymeister-frontend/layouts/frontend/partymeister.blade.php
@@ -10,75 +10,33 @@
     @yield('view-styles')
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <style type="text/css">
-        body {
-            background: transparent;
-            font-family: 'Roboto Condensed', sans-serif;
-        }
-        p, h1, h2, h3, h4, h5, h6 {
-            font-family: 'Roboto Condensed', sans-serif !important;
-        }
-        h1, h2, h3, h4, h5, h6 {
-            text-align: center;
-        }
-        .grid-container {
-            opacity: 0.95;
-            max-width: 600px;
-        }
-        .full-content {
-            border-radius: 20px;
-            background: #faf4e7;
-            position: relative;
-            top: 200px;
-            padding: 1.5rem;
-            margin-bottom: 6.5rem;
-            box-shadow: 5px 5px 5px #353739;
-            border: 1px solid #353739;
-
-        }
         html {
             background: url(/images/frontend/revision-2020-bg.jpg) no-repeat center center fixed;
             background-size: cover;
         }
         #headline {
-            width: 25%;
-            min-width: 200px;
-            position: fixed;
-            top: 5%;
-            left: 5%;
-        }
-        #subline {
-            position: fixed;
-            right: 5%;
-            text-align: right;
+            width: 40%;
+            min-width: 270px;
+            position: absolute;
             top: 10%;
+            left: 10%;
+        }
+        #footer {
+            position: absolute;
+            left: 15%;
+            bottom: 10%;
             font-size: 30px;
             color: #f6e9d1;
             font-family: 'Roboto Condensed', sans-serif;
         }
-        #subline small {
+        #footer small {
             font-size: 20px;
-        }
-        .footer {
-            background: #fcf9f3;
-            opacity: 0.95;
-            height: 3rem;
-            padding: 0;
-        }
-        .footer ul.menu a {
-            font-family: 'Roboto Condensed', sans-serif !important;
-        }
-        .button.success {
-            background-color: #71854d;
-            color: white;
-        }
-        .callout.primary {
-            background-color: #c2cfd4;
         }
     </style>
 </head>
 <body>
 <img src="/images/frontend/revision-2020-headline.png" id="headline">
-<p id="subline">
+<p id="footer">
     Revision - April 10th to 13th 2020<br>
 </p>
 {{--@include('motor-cms::layouts.frontend.partials.navigation')--}}
@@ -88,8 +46,6 @@
 <div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%;">
     <ul class="menu align-center">
         <li><a href="https://2019.revision-party.net/privacy_policy">Privacy policy</a></li>
-        <li><a href="https://2019.revision-party.net/privacy_policy">Contact</a></li>
-        <li><a href="https://2019.revision-party.net/privacy_policy">Imprint</a></li>
     </ul>
 </div>
 <script src="{{mix('js/partymeister-frontend.js')}}"></script>
