f7f4c7a983e3caa905da686057f03379c6650a2a
Reza Esmaili <bitbucket@dfox.info>
2020-01-28 23:08:27 +0100

    Fake image width

---
+++ resources/views/vendor/motor-cms/frontend/components/text.blade.php
@@ -0,0 +1,21 @@
+<h1>{{$component->headline}}</h1>
+@if (is_null($file))
+    {!! $component->body !!}
+@else
+    @if ($position === 'top')
+        <div class="thumbnail">
+            <a href="{{$file}}"><img src="{{$file}}" alt="{{$description}}"></a>
+        </div>
+        {!! $component->body !!}
+    @elseif($position === 'bottom')
+        {!! $component->body !!}
+        <div class="thumbnail">
+            <a href="{{$file}}"><img src="{{$file}}" alt="{{$description}}"></a>
+        </div>
+    @else
+        <div class="thumbnail float-{{$position}}">
+            <a href="{{$file}}"><img src="{{$file}}" alt="{{$description}}"></a>
+        </div>
+        {!! $component->body !!}
+    @endif
+@endif
