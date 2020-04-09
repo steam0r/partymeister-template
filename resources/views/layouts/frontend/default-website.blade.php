de6131a548303a928f24e3c06c4e35943ab78031
Reza Esmaili <github@dfox.info>
2020-04-09 21:41:22 +0200

    Add sponsor footer

--- resources/views/layouts/frontend/default-website.blade.php
+++ resources/views/layouts/frontend/default-website.blade.php
@@ -83,6 +83,8 @@
             opacity: 1;
             height: 3rem;
             padding: 0;
+            position: relative !important;
+            opacity: 0.9;
         }
         .top-bar {
             justify-content: center;
@@ -166,7 +168,10 @@
 <div class="grid-container" style="margin-bottom: 8rem;">
     @include('motor-cms::layouts.frontend.partials.template-sections', ['rows' => $template['items']])
 </div>
-<div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%;">
+<div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%; height: auto; padding-top: 20px;">
+    @foreach (\Motor\Revision\Models\Sponsor::where('is_active', true)->orderBy('level', 'ASC')->orderBy('sort_position', 'ASC')->get() as $sponsor)
+        <a href="{{$sponsor->url}}"><img src="{{$sponsor->file_associations()->where('identifier', 'logo_small')->first()->file->getFirstMedia('file')->getUrl('thumb')}}" style="max-height: 50px; max-width: 200px;"></a>
+    @endforeach
     <ul class="menu align-center">
         <li><a href="/privacy">Privacy policy</a></li>
         <li><a href="/contact">Contact and Imprint</a></li>
