da45b158c279d535ad584978337b5215a369049d
Reza Esmaili <github@dfox.info>
2020-04-12 14:39:18 +0200

    Generate assets

--- resources/assets/js/frontend.js
+++ resources/assets/js/frontend.js
@@ -1,2 +1,3 @@
 require('partymeister-frontend/resources/assets/js/partymeister-frontend/main');
+require('partymeister-slides/resources/assets/js/playlist-viewer/main');
 require('foundation-datepicker');
