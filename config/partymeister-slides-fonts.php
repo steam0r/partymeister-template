26ddcaea0c95619f46ee339f11ed1e384d3519b9
Reza Esmaili <github@dfox.info>
2020-07-03 11:00:04 +0200

    Add new fonts, disable revision shader, install ffmpeg in docker

--- config/partymeister-slides-fonts.php
+++ config/partymeister-slides-fonts.php
@@ -17,5 +17,15 @@ return [
             'path'   => '/vendor/fonts/Lelo.css',
             'family' => '\'Lelo\''
         ],
+        [
+            'name'   => 'norwester',
+            'path'   => '/vendor/fonts/Norwester.css',
+            'family' => '\'Norwester\''
+        ],
+        [
+            'name' => 'pacifico',
+            'path' => '',
+            'family' => '\'Pacifico\''
+        ]
     ],
 ];
