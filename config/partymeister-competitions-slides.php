5191fffa17c453612e2094e2ccc3096b036a321e
Reza Esmaili <github@dfox.info>
2020-07-03 14:41:47 +0200

    Generate assets. configure slide transition

--- config/partymeister-competitions-slides.php
+++ config/partymeister-competitions-slides.php
@@ -3,28 +3,29 @@
 return [
     'video_1'             => [
         'duration'             => 53,
-        'transition'           => 0,
+        'transition'           => 1,
         'transition_duration'  => 500,
         'is_advanced_manually' => 0,
         'midi_note'            => 100,
     ],
     'video_2'             => [
         'duration'             => 100,
-        'transition'           => 0,
+        'transition'           => 1,
         'transition_duration'  => 2000,
         'is_advanced_manually' => 1,
         'midi_note'            => 101,
     ],
     'video_3'             => [
         'duration'             => 100,
-        'transition'           => 0,
+        'transition'           => 1,
         'transition_duration'  => 500,
         'is_advanced_manually' => 0,
         'midi_note'            => 0,
     ],
     'entries'             => [
         'slide_type' => 'announce',
-        'midi_note'  => 0
+        'midi_note'  => 0,
+        'transition' => 1,
     ],
     'siegmeister_bars'    => [
         'slide_type' => 'siegmeister_bars',
@@ -38,26 +39,32 @@ return [
     ],
     'entry'               => [
         'slide_type' => 'compo',
-        'midi_note'  => 0
+        'midi_note'  => 0,
+        'transition' => 1,
     ],
     'comingup'            => [
         'slide_type' => 'comingup',
-        'midi_note'  => 99
+        'midi_note'  => 99,
+        'transition' => 1,
     ],
     'now'                 => [
         'slide_type' => 'comingup',
-        'midi_note'  => 102
+        'midi_note'  => 102,
+        'transition' => 1,
     ],
     'end'                 => [
         'slide_type' => 'end',
-        'midi_note'  => 0
+        'midi_note'  => 0,
+        'transition' => 1,
     ],
     'comments'            => [
         'slide_type' => 'end',
-        'midi_note'  => 0
+        'midi_note'  => 0,
+        'transition' => 1,
     ],
     'participants'        => [
         'slide_type' => 'end',
-        'midi_note'  => 0
+        'midi_note'  => 0,
+        'transition' => 1,
     ]
 ];
