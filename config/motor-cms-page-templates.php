0798175487edb7f88f9c711000d5b7ecd9a9054e
Reza Esmaili <github@dfox.info>
2021-02-05 20:42:33 +0100

    Make start page 3 col

--- config/motor-cms-page-templates.php
+++ config/motor-cms-page-templates.php
@@ -44,16 +44,22 @@ return [
 		'items' => [
 			[
 				[
-					'alias'     => 'content-full',
-					'class'     => 'full-content',
-					'container' => 'first-row-content',
-					'width'     => 8
+					'alias'     => 'content-left',
+					'class'     => 'content-left',
+					'container' => 'first-row-left',
+					'width'     => 3
 				],
 				[
-					'alias'     => 'content-sidebar',
-					'class'     => 'content-sidebar',
-					'container' => 'first-row-sidebar',
-					'width'     => 4
+					'alias'     => 'content-center',
+					'class'     => 'content-center',
+					'container' => 'first-row-center',
+					'width'     => 6
+				],
+				[
+					'alias'     => 'content-right',
+					'class'     => 'content-right',
+					'container' => 'first-row-right',
+					'width'     => 3
 				],
 			],
 		]
