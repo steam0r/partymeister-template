2cf40f616ff17108fbe8f4fcd014a88d2212a0f2
Reza Esmaili <github@dfox.info>
2021-03-17 21:27:12 +0100

    Change footer color also in other templates ;)

--- resources/views/layouts/frontend/default-website.blade.php
+++ resources/views/layouts/frontend/default-website.blade.php
@@ -25,7 +25,7 @@
 <div class="grid-container" style="margin-bottom: 8rem;">
     @include('motor-cms::layouts.frontend.partials.template-sections', ['rows' => $template['items']])
 </div>
-<div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%;">
+<div class="columns shrink footer text-center" style="position: fixed; bottom: 0; width: 100%; background: rgba(55, 60, 63, 1)">
     @foreach (\Motor\Revision\Models\Sponsor::where('is_active', true)->orderBy('level', 'ASC')->orderBy('sort_position', 'ASC')->get() as $sponsor)
         <a href="{{$sponsor->url}}"><img src="{{$sponsor->file_associations()->where('identifier', 'logo_small')->first()->file->getFirstMedia('file')->getUrl('thumb')}}" style="max-height: 50px; max-width: 200px;"></a>
     @endforeach
