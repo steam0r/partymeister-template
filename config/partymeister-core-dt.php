b7d83a550e412115dbfc020a511485793357daa6
Reza Esmaili <github@dfox.info>
2021-03-24 16:04:56 +0100

    Add shop config for deinetickets

--- config/partymeister-core-dt.php
+++ config/partymeister-core-dt.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'base_url' => env('DT_BASE_URL'),
-    'email' => env('DT_EMAIL'),
-    'password' => env('DT_PASSWORD')
+	'base_url' => env('DT_BASE_URL'),
+	'email'    => env('DT_EMAIL'),
+	'password' => env('DT_PASSWORD'),
+	'shop'     => env('DT_SHOP'),
 ];
