dc0c8ad3f48531079087cfb3ac2dd0fa26fcc398
Reza Esmaili <bitbucket@dfox.info>
2021-06-06 18:33:13 +0200

    Updated asset builder + generate assets

--- resources/assets/js/frontend.js
+++ resources/assets/js/frontend.js
@@ -1,3 +1,3 @@
-require('partymeister-frontend/resources/assets/js/partymeister-frontend/main');
-require('partymeister-slides/resources/assets/js/playlist-viewer/main');
-require('foundation-datepicker');
+// require('./vendor/partymeister/frontend/resources/assets/js/partymeister-frontend/main');
+// require('./vendor/partymeister/slides/resources/assets/js/playlist-viewer/main');
+// require('foundation-datepicker');
