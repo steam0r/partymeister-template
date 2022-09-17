7553c2e1e41a86e5a2d792176f03454b6eadf2f0
Reza Esmaili <bitbucket@dfox.info>
2022-09-17 21:05:56 +0200

    Add fonts; regenerate assets

--- config/partymeister-slides-fonts.php
+++ config/partymeister-slides-fonts.php
@@ -17,5 +17,10 @@ return [
             'path'   => '/fonts/krona.css',
             'family' => '\'Krona One\'',
         ],
+        [
+            'name'   => 'Kanit',
+            'path'   => 'https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400;1,500&display=swap',
+            'family' => '\'Kanit\'',
+        ],
     ],
 ];
