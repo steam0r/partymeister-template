4956837b408f3d37a520162d2a31575dfe9fc841
Tammo kb Hinrichs <kb@kebby.org>
2022-12-25 21:41:30 +0100

    Trying to add the Lato font...

--- config/partymeister-slides-fonts.php
+++ config/partymeister-slides-fonts.php
@@ -22,5 +22,10 @@ return [
             'path'   => 'https://fonts.googleapis.com/css2?family=Literata:opsz,wght@7..72,500;7..72,600;7..72,700&family=Syne:wght@400;500',
             'family' => '\'Literata\'',
         ],
+        [
+            'name'   => 'Lato',
+            'path'   => 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900',
+            'family' => '\'Lato\'',
+        ],
     ],
 ];
